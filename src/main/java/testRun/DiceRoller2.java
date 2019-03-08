5640d3d8f423bacda5a2658e93adfd4d479ccfb6
Dorian Abraham <dabraham3@bostonk12.org>
2019-03-07 19:11:00 -0500

    We made it look pretty and readable and give you stats for your future games if you want

--- src/main/java/testRun/DiceRoller2.java
+++ src/main/java/testRun/DiceRoller2.java
@@ -15,19 +15,25 @@ public class DiceRoller2 {
             "INTELLIGENCE", 
             "WISDOM", 
             "CHARISMA"
-    );
+            );
     final static List<String> people = List.of(
             "Xavier",
             "Dorian",
             "Will",
             "Collin"
-    );
+            );
+    final static List<String> gameNumbers = List.of(
+            "game 1",
+            "game 2",
+            "game 3",
+            "game 4"
+            );
 
     private static int getRandomBetweenOneAnd(int bound) {
         Random dice = new Random();
         return dice.nextInt(bound) + 1; 
     }
-    
+
     private static int rollDice(int sides, int numberOfDice) {
         int total = 0;
         for (int i = 0; i < numberOfDice; i++) {
@@ -36,19 +42,17 @@ public class DiceRoller2 {
         }
         return total;
     }
-    
+
     public static void main(String[] args) {
-        for (final String person : people){
-            System.out.print(person);
-            System.out.println(":");
-            for (final String stat : stats) {
-                System.out.println(String.format("%-14s: %s", stat, rollDice(sides, numberOfDice)));
+        for (final String gameNum : gameNumbers) {
+            System.out.println(gameNum + ":");
+            for (final String person : people) {
+                System.out.println(String.format("      %s:", person));
+                for (final String stat : stats) {
+                    System.out.println(String.format("\t\t%-16s: %s", stat, rollDice(sides, numberOfDice)));
+                }
+                System.out.println();
             }
-            System.out.println();
         }
-        
-        
-        
-        
     }
 }
