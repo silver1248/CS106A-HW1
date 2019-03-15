95ca471c0c3f2455be65ae8614c0edff303b6be9
Dorian Abraham <dabraham02126@gmail.com>
2019-03-15 15:36:18 -0400

    I did 30 minutes of coding

---
+++ src/main/java/testRun/weirdCounting.java
@@ -0,0 +1,20 @@
+package testRun;
+
+import java.util.Random;
+
+public class weirdCounting {
+    private static void weirdCounting() {
+        for (i = 1; i < 10; i++());{
+            System.out.println(i);
+            for(j = 1; j > i; j++) {                
+                System.out.println();
+            }
+            }
+        }
+    
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
