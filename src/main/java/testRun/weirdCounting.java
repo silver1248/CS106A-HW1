472445a390a7f266b52a70eec20b58ad7d55d0b6
Dorian Abraham <dabraham3@bostonk12.org>
2019-03-16 18:34:09 -0400

    got rid of comments

--- src/main/java/testRun/weirdCounting.java
+++ src/main/java/testRun/weirdCounting.java
@@ -4,9 +4,7 @@ package testRun;
 public class weirdCounting {
     private static void count() {
         for (int i = 1; i <= 10; i++) {
-//            System.out.println();
             System.out.println(i);
-//            System.out.println();
             for(int j = 1; j <= i; j++) {
                 System.out.println("\t" + j);
             }
